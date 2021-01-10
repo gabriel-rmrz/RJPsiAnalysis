64bb0ec5a30ab0bd91cff6139d82560390a2697e
gabriel-rmrz <gabriel-rmrz@users.noreply.github.com>
2021-01-10 01:56:09 +0100

    adding the repository to the src directory

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo Hello
-#set -ex
+set -ex
 
-#shopt -s expand_aliases
-#set +u && source ${CMS_PATH}/cmsset_default.sh; set -u
-#export SCRAM_ARCH=${SCRAM_ARCHITECTURE}
-#cmsrel ${CMSSW_RELEASE}
-#cd ${CMSSW_RELEASE}/src
-#cmsenv
-#
-#git cms-init
-#git cms-merge-topic -u friti:TransientTracks
-#git cms-merge-topic -u friti:KinParticleVtxFitter
-#git clone git@github.com:gabriel-rmrz/RJPsiAnalysis.git ./PhysicsTools
-#mv ${CI_PROJECT_DIR}
-#git cms-addpkg PhysicsTools/NanoAOD
-#scram b
+shopt -s expand_aliases
+set +u && source ${CMS_PATH}/cmsset_default.sh; set -u
+export SCRAM_ARCH=${SCRAM_ARCHITECTURE}
+cmsrel ${CMSSW_RELEASE}
+cd ${CMSSW_RELEASE}/src
+cmsenv
+
+git cms-init
+git cms-merge-topic -u friti:TransientTracks
+git cms-merge-topic -u friti:KinParticleVtxFitter
+git clone git@github.com:gabriel-rmrz/RJPsiAnalysis.git ./PhysicsTools
+mv ${CI_PROJECT_DIR}/RJPsiNano ${CMSSW_BASE}/src/PhysicsTools
+git cms-addpkg PhysicsTools/NanoAOD
+scram b
